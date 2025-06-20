b04bab40647684b06650302692e62ebd10f51464
Jason Garcia <jagarcia7354@outlook.com>
2025-06-20 12:02:23 -0400

    Completed lesson 06repeatstring.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0){
+        return "ERROR";
+        
+    }
+    let i = 0;
+    let repeatedWord = "";
+    while (i < num){
+        repeatedWord = repeatedWord + str;
+        i++;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
