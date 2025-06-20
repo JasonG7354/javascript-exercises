85be830c243ea77df3314c474b368afa55ff0244
Jason Garcia <jagarcia7354@outlook.com>
2025-06-20 14:56:36 -0400

    Completed reverseStrings excercise.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+     let arr = str.split('');
+  let arrReverse = [];
+  for (let i = 0; i < arr.length; i++) {
+    let removedNumber = arr.pop();
+    arr.unshift(removedNumber)
+    arrReverse.push(removedNumber);
+  }
 
+  return arrReverse.join('');
 };
 
 // Do not edit below this line
