95500cd8a057f45cce25778438225a0e31b57c78
Jason Garcia <jagarcia7354@outlook.com>
2025-06-22 10:15:46 -0400

    Completed lesson 08RemovefromArray.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...values) {
+    newArray = [];
+    for (let i = 0; i < array.length; i++){
+        let matchFound = false;
+        for (let j = 0; j < values.length; j++){
+            if (array[i] === values[j]){
+                matchFound = true;
+            }
+        }
+        if (matchFound === false){
+             newArray.push(array[i]);
+        }
+        }
+        return newArray;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
