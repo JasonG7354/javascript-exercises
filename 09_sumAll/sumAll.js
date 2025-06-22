0930044b9ec4a79b75fc536cb1ebe357e67aba94
Jason Garcia <jagarcia7354@outlook.com>
2025-06-22 10:38:51 -0400

    Completed Lesson 09sumAll.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let newArray = [];
+    if (num1 < 0 || num2 < 0 || typeof num1 !== typeof 1 || typeof num2 !== typeof 1 || num1 % 1 !== 0 || num2 % 1 !== 0){
+        return "ERROR";
+    }
+    if (num1 < num2){
+        while (num1 <= num2){
+            newArray.push(num1);
+            num1++;
+        }
+        for (let i = 0; i < newArray.length; i++){
+            sum = sum + newArray[i];
+        }
+    } else if (num1 > num2){
+        while (num1 >= num2){
+            newArray.push(num1);
+            num1--;
+        }
+        for (let i = 0; i < newArray.length; i++){
+            sum = sum + newArray[i];
+        }
+    };
+    return sum;
 
 };
 
